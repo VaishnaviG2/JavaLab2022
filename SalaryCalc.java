3908ac81f7c5eea6a70bda0b80a26dc5547903a6
VaishnaviG2 <113009370+VaishnaviG2@users.noreply.github.com>
2022-09-15 15:59:21 +0530

    Add files via upload

---
+++ SalaryCalc.java
@@ -0,0 +1,11 @@
+
+public class SalaryCalc {
+	public static void main(String[] args) {
+	    int a=30000;
+	    int x=11000;
+	    int y=5000;
+	    int z=a-x-y;
+	    System.out.print("salary of the third employee"+" "+z);
+
+	    }
+}
